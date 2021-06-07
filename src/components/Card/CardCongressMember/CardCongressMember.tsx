8a2f1e505ad4fe1b61a89e47286e06a09151a2ea
William Takayama <william-sobreira@hotmail.com>
2021-06-06 23:21:28 -0300

    :fix must use history instead of link

--- src/components/Card/CardCongressMember/CardCongressMember.tsx
+++ src/components/Card/CardCongressMember/CardCongressMember.tsx
@@ -1,13 +1,8 @@
-import React, { useCallback, useState } from "react";
 import { FaFacebook, FaTwitter, FaYoutube } from "react-icons/fa";
-import { Link, useHistory } from "react-router-dom";
-import { congressService } from "../../../services/congress.service";
+import { useHistory } from "react-router-dom";
 import { Chamber } from "../../../types/Congress";
-import { CongressMemberDetail } from "../../../types/CongressMemberDetail";
-import { LeavingOfficeMember } from "../../../types/CongressMemberLeavingOffice";
 import { Member } from "../../../types/CongressMembers";
 import { Button } from "../../Button/Button";
-import { ModalMemberDetails } from "../../ModalMemberDetails/ModalMemberDetails";
 import { Spacing } from "../../Spacing/Spacing";
 import { Typography } from "../../Typography/Typography";
 import { Card, CardContent } from "../Card";
@@ -46,49 +41,53 @@ export function CardCongressMember({
   session: number;
   chamber: Chamber;
 }) {
+  const history = useHistory();
+
   return (
     <>
-      <Link
-        to={{
-          pathname: `/member/${member.id}`,
-          search: `?chamber=${chamber}&session=${session}`,
+      <Card
+        height={90}
+        color="var(--color-x-contrast)"
+        data-role="button"
+        onClick={async () => {
+          await history.push(`/member/${member.id}`, {
+            search: `?session=${String(session)}&chamber=${chamber}`,
+          });
         }}
       >
-        <Card height={90} color="var(--color-x-contrast)" data-role="button">
-          <CardContent alignDirection="column">
-            <Typography type="body-heading">
-              <b>Fullname:</b> {member.first_name} {member.last_name}
-            </Typography>
+        <CardContent alignDirection="column">
+          <Typography type="body-heading">
+            <b>Fullname:</b> {member.first_name} {member.last_name}
+          </Typography>
 
-            <Typography type="body-heading">
-              <b>Gender:</b> {member.gender === "F" ? "Female" : "Male"}
-            </Typography>
+          <Typography type="body-heading">
+            <b>Gender:</b> {member.gender === "F" ? "Female" : "Male"}
+          </Typography>
 
-            <Typography type="body-heading">
-              <b>State:</b> {member.state}
-            </Typography>
-          </CardContent>
+          <Typography type="body-heading">
+            <b>State:</b> {member.state}
+          </Typography>
+        </CardContent>
 
-          <CardContent justifyContent="flex-end">
-            {SOCIAL_MEDIAS.map((media) => {
-              if (member[media.key as keyof Member] == null) {
-                return undefined;
-              }
-              return (
-                <Button
-                  key={media.key}
-                  size="xx-small"
-                  bordered={false}
-                  style={{ background: "transparent" }}
-                  href={`${media.baseUrl}/${member[media.key as keyof Member]}`}
-                >
-                  {media.icon}
-                </Button>
-              );
-            })}
-          </CardContent>
-        </Card>
-      </Link>
+        <CardContent justifyContent="flex-end">
+          {SOCIAL_MEDIAS.map((media) => {
+            if (member[media.key as keyof Member] == null) {
+              return undefined;
+            }
+            return (
+              <Button
+                key={media.key}
+                size="xx-small"
+                bordered={false}
+                style={{ background: "transparent" }}
+                href={`${media.baseUrl}/${member[media.key as keyof Member]}`}
+              >
+                {media.icon}
+              </Button>
+            );
+          })}
+        </CardContent>
+      </Card>
 
       <Spacing type="block" size="x-small" />
     </>
